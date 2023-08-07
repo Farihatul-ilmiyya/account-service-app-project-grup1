b09acf53d4f68e4ae3f4623ba2f4e830463a832b
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-08-07 23:07:33 +0700

    repair code

--- entity/users_entity.go
+++ entity/users_entity.go
@@ -7,10 +7,10 @@ type Users struct {
 	Username    string
 	Email       string
 	Password    string
-	PhoneNumber int
+	PhoneNumber string
 	DateOfBirth time.Time
 	Address     string
-	Balance     int
+	Balance     float64
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
 	DeletedAt   time.Time
