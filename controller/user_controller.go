5c8070e89cb6bb35fb350b0de7520bb5df8aba48
Farihatul Ilmiyya <farihatulilmiyya05@gmail.com>
2023-08-08 20:02:38 +0700

    feature register

--- controller/user_controller.go
+++ controller/user_controller.go
@@ -4,15 +4,47 @@ import (
 	"account-service-app/entity"
 	"account-service-app/helpers"
 	"database/sql"
-	"errors"
+
+	"github.com/google/uuid"
 )
 
 func RegisterAccount(db *sql.DB, user entity.Users) (string, error) {
-	passHashing := helpers.HashPassword(user.Password)
-	_, err := db.Exec("INSERT INTO users(id, username, email, password, phone_number, date_of_birth, address, balance) VALUE (?, ?, ?, ?, ?, ?, ?, ?)", user.ID, user.Username, user.Email, passHashing, user.PhoneNumber, user.DateOfBirth, user.Address, user.Balance)
+	Uuid := uuid.New()
+	//validating email
+	emailIsValid, err := helpers.ValidationEmail(user.Email)
+	if !emailIsValid {
+		return "", err
+	}
+
+	//validating password
+	passwordIsValid, err := helpers.ValidationPassword(user.Password)
+	if !passwordIsValid {
+		return "", err
+	}
+	passHashing := ""
+	if passwordIsValid {
+		passHashing = helpers.HashPassword(user.Password)
+	} else {
+		return "", err
+	}
+
+	//validating phonenumber
+	phoneNumberIsValid, err := helpers.ValidationPhoneNumber(user.PhoneNumber)
+	if !phoneNumberIsValid {
+		return "", err
+	}
+
+	//validating dateofbirth
+	isValid, birthdate, err := helpers.ValidationDateofBirth(user.DateOfBirth)
+	if !isValid {
+		return "", err
+	}
+	_, err = db.Exec("INSERT INTO users(id, username, email, password, phone_number, date_of_birth, address, balance) VALUE (?, ?, ?, ?, ?, ?, ?, ?)", Uuid, user.Username, user.Email, passHashing, user.PhoneNumber, birthdate, user.Address, user.Balance)
+
 	defer db.Close()
+
 	if err != nil {
-		return "", errors.New("[FAILED] Failed to register account")
+		return "", err
 	}
 
 	outputStr := "\n[SUCCESS] Account registered successfully.\n\n"
--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.20
 
 require (
 	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	golang.org/x/crypto v0.12.0 // indirect
 )
